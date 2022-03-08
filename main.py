f9a8663cc11ca48873acb4d3bcb344094f604057
pieterhop <pieterhoppenbrouwers@hotmail.com>
2022-03-08 21:42:25 +0200

    Add constants for easy adjusting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from __future__ import division
 from __future__ import print_function
-from audioop import add
 from ortools.constraint_solver import routing_enums_pb2
 from ortools.constraint_solver import pywrapcp
 import requests
@@ -8,6 +7,11 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
+
+NUM_VEHICLES = 10
+MAX_WAITING_TIME = 30
+MAX_VEHICLE_TIME = 8 * 60
+PENALTY_VALUE = 1000
 
 
 class Route:
@@ -57,7 +61,7 @@ class Route:
             (10, 15),  # 15
             (11, 15),  # 16
         ]
-        data['num_vehicles'] = 4
+        data['num_vehicles'] = NUM_VEHICLES
         data['depot'] = 0
         return data
 
@@ -99,40 +103,30 @@ class Route:
 
     def main(self):
         """Solve the VRP with time windows."""
-        # Instantiate the data problem.
         data = self.create_data_model()
-
-        # Create the routing index manager.
-        manager = pywrapcp.RoutingIndexManager(len(data['time_matrix']),
-                                            data['num_vehicles'], data['depot'])
-
-        # Create Routing Model.
+        manager = pywrapcp.RoutingIndexManager(len(data['time_matrix']), data['num_vehicles'], data['depot'])
         routing = pywrapcp.RoutingModel(manager)
 
-
-        # Create and register a transit callback.
         def time_callback(from_index, to_index):
             """Returns the travel time between the two nodes."""
-            # Convert from routing variable Index to time matrix NodeIndex.
             from_node = manager.IndexToNode(from_index)
             to_node = manager.IndexToNode(to_index)
             return data['time_matrix'][from_node][to_node]
 
         transit_callback_index = routing.RegisterTransitCallback(time_callback)
-
-        # Define cost of each arc.
         routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
 
-        # Add Time Windows constraint.
+        # Add Time Windows constraint
         time = 'Time'
         routing.AddDimension(
             transit_callback_index,
-            30,  # allow waiting time
-            8 * 60,  # maximum time per vehicle
-            False,  # Don't force start cumul to zero.
+            MAX_WAITING_TIME,  # allow waiting time, default 30
+            MAX_VEHICLE_TIME,  # maximum time per vehicle, default 30
+            False,  # Don't force start cumul to zero, deafult False
             time)
 
-        penalty = 100000000
+        # Add Penalty constraint
+        penalty = PENALTY_VALUE
         for node in range(1, len(data['time_matrix'])):
             routing.AddDisjunction([manager.NodeToIndex(node)], penalty)
 
@@ -158,14 +152,11 @@ class Route:
             routing.AddVariableMinimizedByFinalizer(
                 time_dimension.CumulVar(routing.End(i)))
 
-        # Setting first solution heuristic.
         search_parameters = pywrapcp.DefaultRoutingSearchParameters()
         search_parameters.first_solution_strategy = (routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC)
 
-        # Solve the problem.
         solution = routing.SolveWithParameters(search_parameters)
 
-        # Print solution on console.
         if solution:
             self.print_solution(data, manager, routing, solution)
         else:
