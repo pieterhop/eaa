d837b7e33bf14b19ab0334effd121b77d8465dcc
pieterhop <pieterhoppenbrouwers@hotmail.com>
2022-03-21 17:27:00 +0100

    Styling

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, url_for, flash, redirect
+from scipy.fftpack import shift
 from engine import Route
 
 app = Flask(__name__)
@@ -8,17 +9,22 @@ app.config['SECRET_KEY'] = 'your secret key'
 def index():
     if request.method == 'POST':
         number = int(request.form['numberValue'])
+        addresses = []
+        shifts = []
+        for i in range(0, number):
+            address = str(request.form[f'address{i}'])
+            if len(address) > 0:
+                addresses.append(address)
+            start = int(request.form[f'start{i}'])
+            end = int(request.form[f'end{i}'])
+            shift_range = (start, end)
+            shifts.append(shift_range)
         data = {
             'vehicles': int(request.form['vehicles']),
             'waitingtime': int(request.form['waitingtime']),
             'maxshifttime': int(request.form['maxshifttime'])
         }
-        addresses = []
-        for i in range(0, number):
-            address = str(request.form[f'address{i}'])
-            if len(address) > 0:
-                addresses.append(address)
-        res = Route(addresses, data).addresses
+        res = Route(addresses, shifts, data).addresses
         return render_template('index.html', res=res)
 
     return render_template('index.html')
